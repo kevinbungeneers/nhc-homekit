2d859c8c92af90ba848f45ef3a28144a651f2682
Kevin Bungeneers <kevin.bungeneers@gmail.com>
2017-01-01 23:50:18 +0100

    Load actions from NHC Client and add them to the bridge

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-var HAP = require('hap-nodejs');
-var Accessory = HAP.Accessory;
-var HomeControlBridge = require('./lib/HomeControlBridge');
+'use strict';
 
-HAP.init();
+const HomeControlClient = require('nhc-client').HomeControlClient;
+const Accessory = require('hap-nodejs').Accessory;
+const init = require('hap-nodejs').init;
+var HomeControlBridge = require('./lib/HomeControlBridge');
+var Light = require('./lib/accessories/Light');
+var AccessoryFactory = require('./lib/AccessoryFactory');
+
+init();
 
 var bridge = new HomeControlBridge();
+var client = new HomeControlClient();
+client.connect('192.168.0.226');
 
-bridge.on('identify', function (paired, callback) {
-    console.log('NHC bridge identify');
-    callback();
-});
-
-bridge.publish({
-    username: "CC:22:3D:E3:CE:30",
-    port: 51826,
-    pincode: '031-45-154'
+client.on('connect', function() {
+  client.listActions(function(actions) {
+    // var light = new Light(actions[1]);
+    // bridge.addBridgedAccessory(
+    //   AccessoryFactory.createLightAccessory(light)
+    // );
+    actions.forEach(function(action) {
+      var light = new Light(action);
+      bridge.addBridgedAccessory(
+        AccessoryFactory.createLightAccessory(light)
+      );
+    });
+    bridge.publish({
+        username: "CC:22:3D:E3:CE:30",
+        port: 51826,
+        pincode: '031-45-154'
+    });
+  });
 });
